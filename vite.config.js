ec69c5695372ea56019a97189b1a8bfedda48c65
lerghin <lerghincardenas@gmail.com>
2024-06-07 10:48:12 -0400

    main

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Directorio de salida para los archivos compilados
-    sourcemap: true // Opcional, para facilitar la depuración
-  }
+  
 })
