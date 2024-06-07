70d9e9cdbe32d9408b4bd2ec3c6cbb19980978d8
lerghin <lerghincardenas@gmail.com>
2024-06-07 10:37:52 -0400

    p8

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Asegúrate de que esto coincida con `distDir` en vercel.json
+    outDir: 'dist', // Directorio de salida para los archivos compilados
+    sourcemap: true // Opcional, para facilitar la depuración
   }
 })
